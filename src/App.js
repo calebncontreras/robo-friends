dca775168ae6d074828ca9b964d24d3991e2e09f
calebncontreras <calebncontreras5@gmail.com>
2020-04-16 16:43:36 -0700

    implemented SearchBox with card filtering capabilities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CardList from "./components/CardList";
 import { robotsArray } from "./robots";
-
+import SearchBox from "./components/SearchBox";
 class App extends React.Component {
   constructor() {
     super();
@@ -13,14 +13,21 @@ class App extends React.Component {
 
   componentDidMount = () => this.setState({ robots: robotsArray });
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
   render() {
-    const { robots } = this.state;
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
+    });
     return !robots.length ? (
       <h1>Loading</h1>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <CardList robots={robots} />
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
