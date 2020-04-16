ffcb8d939f3fa9f76a40cb47f49bf458830281af
calebncontreras <calebncontreras5@gmail.com>
2020-04-16 16:25:56 -0700

    implemented Card and CardList Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import CardList from "./components/CardList";
+import { robotsArray } from "./robots";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App is runnning</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount = () => this.setState({ robots: robotsArray });
+
+  render() {
+    const { robots } = this.state;
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <CardList robots={robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
