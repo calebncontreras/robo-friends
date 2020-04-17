484b32c5e414710006a0ae8e5aa00173716661e2
calebncontreras <calebncontreras5@gmail.com>
2020-04-17 11:19:30 -0700

    added Scroll Component, added styling, and reorganized file structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,33 @@
 import React from "react";
-import CardList from "./components/CardList";
-import { robotsArray } from "./robots";
-import SearchBox from "./components/SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       robots: [],
       searchField: "",
+      error: {},
     };
   }
 
-  componentDidMount = () => this.setState({ robots: robotsArray });
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value });
   };
   render() {
-    const { robots, searchField } = this.state;
+    const { robots, searchField, error } = this.state;
+    console.log(error);
     const filteredRobots = robots.filter((robot) => {
       return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
     });
@@ -27,7 +37,9 @@ class App extends React.Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
